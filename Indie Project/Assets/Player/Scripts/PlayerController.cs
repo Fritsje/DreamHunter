d049f36c41cf79f036448d650f5faaf18d4047d1
Donny <Donnyy@hotmail.co.jp>
2016-11-15 14:05:18 +0100

    Starting the game ..

---
+++ Indie Project/Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 10.0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HorizontalMovement ();
+	}
+
+	private void HorizontalMovement()
+	{
+		float translation = Input.GetAxis("Horizontal") * speed;
+		translation *= Time.deltaTime;
+		transform.Translate(translation, 0, 0);
+	}
+}
